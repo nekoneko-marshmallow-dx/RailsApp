8af26ff2b5fefc16a63122260a1db10f8cb04ede
瀧本 悠太 <takimoto.y@dentsudigital.co.jp>
2024-10-13 18:10:54 +0900

    'ページの表示＆動作確認'

--- Gemfile
+++ Gemfile
@@ -4,6 +4,9 @@ source "https://rubygems.org"
 gem "rails", "~> 7.2.1"
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
+# Sassのエントリー？を追加
+gem 'sassc-rails'
+
 # Use sqlite3 as the database for Active Record
 gem "sqlite3", ">= 1.4"
 # Use the Puma web server [https://github.com/puma/puma]
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "msgboard/index"
+  get 'hello/index'
+  get 'hello', to: 'hello#index'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  #新しいアクションを追加したら、ルート情報を追加
+  post 'hello', to: 'hello#index'
+  post 'hello/index'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
